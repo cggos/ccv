1a3bb01582402c8c27f4cf6563ffad0af32ad362
GaoHongchen <1656738573@qq.com>
2019-04-22 09:05:50 +0800

    update camera_roswrapper

--- cv_cam/camera_roswrapper/src/camera_polling.cpp
+++ cv_cam/camera_roswrapper/src/camera_polling.cpp
@@ -4,86 +4,83 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/highgui/highgui.hpp>
 
-class CameraDriver
-{
-    public:
-        enum Default {
-            DEFAULT_CAMERA_INDEX = 0,
-            DEFAULT_FPS = 30
-        };
+class CameraDriver {
+public:
+    enum Default {
+        DEFAULT_CAMERA_INDEX = 0,
+        DEFAULT_FPS = 30
+    };
 
-        CameraDriver( int camera_index = DEFAULT_CAMERA_INDEX ) : 
-            nh( "~" ), 
-            it( nh ), 
-            camera_index(camera_index), 
-            camera( camera_index ), 
-            last( 0 ) {
-                nh.param<int>( "camera_index", camera_index, DEFAULT_CAMERA_INDEX );
-                nh.param<int>( "fps", fps, DEFAULT_FPS );
+    CameraDriver(int camera_index = DEFAULT_CAMERA_INDEX) :
+            nh("~"),
+            it(nh),
+            camera_index(camera_index),
+            camera(camera_index),
+            last(0) {
+        nh.param<int>("camera_index", camera_index, DEFAULT_CAMERA_INDEX);
+        nh.param<int>("fps", fps, DEFAULT_FPS);
 
-                if ( not camera.isOpened() ){
-                    ROS_ERROR_STREAM( "Failed to open camera device!" );
-                    ros::shutdown();
-                }
-
-                period = ros::Duration( 1. / fps );
-
-                pub_image_raw = it.advertise( "image_raw", 1 );
-
-                frame = boost::make_shared< cv_bridge::CvImage >();
-                frame->encoding = sensor_msgs::image_encodings::BGR8;
-            }
-
-        ~CameraDriver() {
-            camera.release();
+        if (not camera.isOpened()) {
+            ROS_ERROR_STREAM("Failed to open camera device!");
+            ros::shutdown();
         }
 
-        void capture() {
+        period = ros::Duration(1. / fps);
 
-            camera >> frame->image;	// blocking, so it frees the CPU.
-            if ( frame->image.empty() ) {
-                ROS_ERROR_STREAM( "Failed to retrive new frame! " );
-                ros::shutdown();
-            }
+        pub_image_raw = it.advertise("image_raw", 1);
 
-            ros::Time now = ros::Time::now();
-            if ( ( now - last ) >= period ) {
-                frame->header.stamp = now;
-                pub_image_raw.publish( frame->toImageMsg() );
+        frame = boost::make_shared<cv_bridge::CvImage>();
+        frame->encoding = sensor_msgs::image_encodings::BGR8;
+    }
 
-                last = now;
-            }
+    ~CameraDriver() {
+        camera.release();
+    }
+
+    void capture() {
+        camera >> frame->image;    // blocking, so it frees the CPU.
+        if (frame->image.empty()) {
+            ROS_ERROR_STREAM("Failed to retrive new frame! ");
+            ros::shutdown();
         }
 
-    private:
-        ros::NodeHandle nh;
-        image_transport::ImageTransport it;
-        image_transport::Publisher pub_image_raw;
+        ros::Time now = ros::Time::now();
+        if ((now - last) >= period) {
+            frame->header.stamp = now;
+            pub_image_raw.publish(frame->toImageMsg());
 
-        int camera_index;
-        int fps;
+            last = now;
+        }
+    }
 
-        cv::VideoCapture camera;
-        cv::Mat image;
-        cv_bridge::CvImagePtr frame;
+private:
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it;
+    image_transport::Publisher pub_image_raw;
 
-        ros::Time last;
-        ros::Duration period;
+    int camera_index;
+    int fps;
+
+    cv::VideoCapture camera;
+    cv_bridge::CvImagePtr frame;
+
+    ros::Time last;
+    ros::Duration period;
 };
 
 
 int main( int argc, char* argv[] )
 {
-    ros::init( argc, argv, "camera" );
+    ros::init(argc, argv, "camera");
 
-    ros::NodeHandle nh( "~" );
+    ros::NodeHandle nh("~");
 
     int camera_index = CameraDriver::DEFAULT_CAMERA_INDEX;
-    nh.param( "camera_index", camera_index, camera_index );
+    nh.param("camera_index", camera_index, camera_index);
 
-    CameraDriver camera_driver( camera_index );
+    CameraDriver camera_driver(camera_index);
 
-    while( ros::ok() ){
+    while (ros::ok()) {
         camera_driver.capture();
         ros::spinOnce();
     }
