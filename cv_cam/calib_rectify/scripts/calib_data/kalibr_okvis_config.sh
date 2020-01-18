b77481e6eef1020fe9e367f87f44b96857a5ee1b
GaoHongchen <1656738573@qq.com>
2020-01-18 22:15:29 +0800

    add calib scripts for Kalibr

---
+++ cv_cam/calib_rectify/scripts/calib_data/kalibr_okvis_config.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+kalibr_okvis_config --cam camchain-imucam-images_imu.yaml --mav mynteye --out config_mynteye_s2110.yaml
--- cv_cam/calib_rectify/scripts/rostopic_throttle.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rosrun topic_tools throttle messages /mynteye/left/image_raw 5 /mynteye_throttle/left/image_raw &
-rosrun topic_tools throttle messages /mynteye/right/image_raw 5 /mynteye_throttle/right/image_raw
