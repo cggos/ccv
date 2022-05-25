75b74434c8fb192fe46f0985f7c40127e854dfde
cggos <cggos@outlook.com>
2022-05-25 14:20:02 +0800

    update RotateScale/RotateScale.cpp

--- libs/opencv/RotateScale/RotateScale.cpp
+++ libs/opencv/RotateScale/RotateScale.cpp
@@ -1,51 +1,47 @@
-#include<opencv2/opencv.hpp>
-#include<cmath>
+#include <cmath>
+#include <opencv2/opencv.hpp>
 
-int main()
-{
-    IplImage *src=cvLoadImage("..\\res\\1.jpg",1);
-    if(src)
-    {
-        IplImage *dst = cvCloneImage(src);
-        cvNamedWindow("src",1);
-        cvMoveWindow("src", 200,200);
-        cvShowImage("src",src);
-                      
-        int angle = 0;
-        for(;;)
-        {                 
-            float m[6];
-            //Matrix m looks like:
-            //
-            //[ m0  m1  m2 ] ==> [ A11 A12 b1 ]
-            //[ m3  m4  m5 ]     [ A21 A22 b2 ]
-            //
-            CvMat M = cvMat(2, 3, CV_32F, m);
-            int w = src->width;
-            int h = src->height;
-            int opt   = 0;//1:Ğı×ªËõ·Å  0£ºĞı×ª
-            double factor;
-            if(opt)         //Ğı×ª¼ÓËõ·Å
-                factor = (cos(angle*CV_PI/180)+1.5)*2;
-            else            //½ö½öĞı×ª
-                factor = 1;
-            m[0] = (float)(factor*cos(angle*CV_PI/180));
-            m[1] = -(float)(factor*sin(angle*CV_PI/180));
-            m[3] =  -m[1];
-            m[4] =  m[0];
-            //½«Ğı×ªÖĞĞÄÒÆÖÁÍ¼ÏñÖĞ¼ä
-            m[2] = w*0.5f;
-            m[5] = h*0.5f;
-            //dst(x,y) = A*src(x,y)+b
-            cvGetQuadrangleSubPix(src, dst, &M);
-            cvNamedWindow("dst",1);
-            cvMoveWindow("dst", 500,200);
-            cvShowImage("dst",dst);
-            if(27 == cvWaitKey(5))
-                break;
-            int delta = 1;
-            angle = (int)(angle + delta)%360;
-        }//for-loop
-    }
-    return 0;
+int main() {
+  IplImage *src = cvLoadImage("../../../data/lena.bmp", 1);
+  if (src) {
+    IplImage *dst = cvCloneImage(src);
+    cvNamedWindow("src", 1);
+    cvMoveWindow("src", 200, 200);
+    cvShowImage("src", src);
+
+    int angle = 0;
+    for (;;) {
+      float m[6];
+      // Matrix m looks like:
+      //
+      //[ m0  m1  m2 ] ==> [ A11 A12 b1 ]
+      //[ m3  m4  m5 ]     [ A21 A22 b2 ]
+      //
+      CvMat M = cvMat(2, 3, CV_32F, m);
+      int w = src->width;
+      int h = src->height;
+      int opt = 0;  // 1:æ—‹è½¬ç¼©æ”¾  0ï¼šæ—‹è½¬
+      double factor;
+      if (opt)  //æ—‹è½¬åŠ ç¼©æ”¾
+        factor = (cos(angle * CV_PI / 180) + 1.5) * 2;
+      else  //ä»…ä»…æ—‹è½¬
+        factor = 1;
+      m[0] = (float)(factor * cos(angle * CV_PI / 180));
+      m[1] = -(float)(factor * sin(angle * CV_PI / 180));
+      m[3] = -m[1];
+      m[4] = m[0];
+      //å°†æ—‹è½¬ä¸­å¿ƒç§»è‡³å›¾åƒä¸­é—´
+      m[2] = w * 0.5f;
+      m[5] = h * 0.5f;
+      // dst(x,y) = A*src(x,y)+b
+      cvGetQuadrangleSubPix(src, dst, &M);
+      cvNamedWindow("dst", 1);
+      cvMoveWindow("dst", 500, 200);
+      cvShowImage("dst", dst);
+      if (27 == cvWaitKey(5)) break;
+      int delta = 1;
+      angle = (int)(angle + delta) % 360;
+    }  // for-loop
+  }
+  return 0;
 }
