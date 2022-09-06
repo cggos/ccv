a97ae018136388a33cc821ada46cb3a6466dff1c
cggos <cggos@outlook.com>
2022-09-06 15:44:59 +0800

    move kalibr scripts to kalibr repo

--- apps/camkit/calib_rectify/scripts/kalibr/view_images.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# rosrun image_view image_view image:=/mynteye/left/image_raw &
-# rosrun image_view image_view image:=/mynteye/right/image_raw
-
-rosrun image_view image_view image:=/T265/fisheye1/image_raw_throttle &
-rosrun image_view image_view image:=/D435I/infra1/image_rect_raw_throttle
